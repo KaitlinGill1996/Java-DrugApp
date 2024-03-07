886561cd7f10318cadf512cae6b33d3891d1930f
Kaitlin Gill <gkaitlin@students.cs.ubc.ca>
2024-03-07 05:12:44 -0800

    Updated tests (only missing JsonWriterTest

--- src/test/persistence/JsonReaderTest.java
+++ src/test/persistence/JsonReaderTest.java
@@ -1,4 +1,52 @@
 package persistence;
 
-public class JsonReaderTest {
+import model.Drug;
+import model.FavouriteDrugs;
+import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+// code inspired by JsonSerializationDemo from CPSC 210
+public class JsonReaderTest extends JsonTest {
+
+    @Test
+    void testReaderNonExistentFile() {
+        JsonReader reader = new JsonReader("./data/noSuchFile.json");
+        try {
+            FavouriteDrugs fd = reader.read();
+            fail("IOException expected");
+        } catch (IOException e) {
+            // pass
+        }
+    }
+
+    @Test
+    void testReaderEmptyFavouriteDrugs() {
+        JsonReader reader = new JsonReader("./data/testReaderFavouriteDrugs.json");
+        try {
+            FavouriteDrugs fd = reader.read();
+            assertEquals(0, fd.getNumDrugs());
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
+    @Test
+    void testReaderGeneralFavouriteDrugs() {
+        JsonReader reader = new JsonReader("./data/testReaderGeneralFavouriteDrugs.json");
+        try {
+            FavouriteDrugs fd = reader.read();
+            List<Drug> drugs = fd.getFavouriteDrugs();
+            assertEquals(2, fd.getNumDrugs());
+            checkDrug("Rosuvastatin", "Crestor", "Statin", 0.39, drugs.get(0));
+            checkDrug("Rabeprazole", "Pariet", "PPI", 0.23, drugs.get(1));
+        } catch (IOException e) {
+            fail("Couldn't read from file");
+        }
+    }
+
 }
--- src/test/persistence/JsonTest.java
+++ src/test/persistence/JsonTest.java
@@ -1,4 +1,15 @@
 package persistence;
 
+import model.Drug;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+//code inspired by JsonSerializationDemo
 public class JsonTest {
+    protected void checkDrug(String genName, String brandName, String drugClass, double price, Drug drug) {
+        assertEquals(genName, drug.getGenName());
+        assertEquals(brandName, drug.getBrandName());
+        assertEquals(drugClass, drug.getDrugClass());
+        assertEquals(price, drug.getUnitCost());
+    }
 }
